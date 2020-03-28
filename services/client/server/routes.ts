66179f6bb62072435e85ad26ff022fb3f3ea49dc
M4lF3s <mfraas64@gmail.com>
2020-03-28 14:18:24 +0100

    cleared nextjs client

--- services/client/server/routes.ts
+++
@@ -1,8 +0,0 @@
-const next_routes = require( 'next-routes' );
-
-// Setup router.
-module.exports = next_routes()
-  .add( 'index', '/' )
-  .add( 'PostsWithClassComponent' )
-  .add( 'PostsWithFunctionComponent' )
-  .add( 'single', '/posts/:slug' );
